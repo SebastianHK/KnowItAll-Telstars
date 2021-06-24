cc40db3489f11236493df89284d38ac76f7152c7
SebastianHK <75618478+SebastianHK@users.noreply.github.com>
2021-06-24 11:55:35 +0200

    Revert "edit form fix"
    This reverts commit d0ca31ced8250d3c8063f02ea8a0d1493519310d.

--- knowitallWebsite/content/admin_control_panel.php
+++ knowitallWebsite/content/admin_control_panel.php
@@ -227,22 +227,22 @@ $numRows = $numRows['COUNT(id)'];
             <p class="tooltip">ID
                 <span class="tooltiptext">ID van het weetje</span>
             </p> -
-            <p class="tooltip">Gebruiker
+            <p class="tooltip">gebruiker
                 <span class="tooltiptext">Naam van gebruiker</span>
             </p> -
-            <p class="tooltip">Titel
+            <p class="tooltip">titel
                 <span class="tooltiptext">Titel van het weetje</span>
             </p> -
-            <p class="tooltip">Geplaatst op
+            <p class="tooltip">plaats datum
                 <span class="tooltiptext">Datum dat het weetje geplaatst is</span>
             </p> -
-            <p class="tooltip">Ingevoerde datum
+            <p class="tooltip">ingevoerde datum
                 <span class="tooltiptext">Datum dat de gebruiker heeft ingevoerd van wanneer het gebeurt is</span>
             </p> -
-            <p class="tooltip">Status
+            <p class="tooltip">status
                 <span class="tooltiptext">Satus van het weetje</span>
             </p> -
-            <p class="tooltip">Edit tools
+            <p class="tooltip">edit tools
                 <span class="tooltiptext">Verwijder of pas een weetje aan</span>
             </p>
         </div>
