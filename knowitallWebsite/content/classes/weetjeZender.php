ed905950ba91d2cfcad67748a3498a1552a404fe
SebastianHK <75618478+SebastianHK@users.noreply.github.com>
2021-06-29 10:55:54 +0200

    plaatjes werken nu

--- knowitallWebsite/content/classes/weetjeZender.php
+++ knowitallWebsite/content/classes/weetjeZender.php
@@ -2,21 +2,23 @@
 class zendWeetje {
     public $titel;
     public $inhoud;
-    public $gebruiker;
     public $ingDatum;
-    public $plaatje;
+    public $gebruiker;
+    public $image;
     public $conn;
 
-    public function __construct($titel, $inhoud, $ingDatum, $plaatje, $gebruiker,$conn)
+    public function __construct($titel, $inhoud, $ingDatum, $image, $gebruiker, $conn)
     {
         $this->titel = $titel;
         $this->inhoud = $inhoud;
         $this->gebruiker = $gebruiker;
         $this->ingDatum = $ingDatum;
-        $this->plaatje = $plaatje;
+        $this->image = $image;
         $this->conn = $conn;
 
-        $sql = "INSERT INTO weetjesdb (titel, weetjes, gebruiker, geb_datum, plaatje) VALUES ('$titel','$inhoud','$gebruiker','$ingDatum','$plaatje')";
+
+
+        $sql = "INSERT INTO weetjesdb (titel, weetjes, gebruiker, geb_datum, plaatje) VALUES ('$titel','$inhoud','$gebruiker','$ingDatum','$image')";
         mysqli_query($conn, $sql);
     }
 }
